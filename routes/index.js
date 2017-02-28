6841ae5870ddbc72761082b8ad205aab2746c518
Matthew Farlymn <matthewfarlymn@gmail.com>
2017-02-28 11:08:08 +0000

    Re-worked views GET/POST and routes JADE

--- routes/index.js
+++ routes/index.js
@@ -4,24 +4,34 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
+    res.render('index', {
+        title: 'Welcome, to generate jokes '
+    });
+
+});
+
+/* GET joke page. */
+router.get('/jokes', function(req, res, next) {
+
     if (!req.session.hasOwnProperty('jokes')) {
         req.session.jokes = [];
     }
 
-    res.render('index', {
+    res.render('jokes', {
         title: 'Enter a joke',
         jokes: req.session.jokes
     });
 
 });
 
+/* POST joke page. */
 router.post('/jokes', function(req, res, next) {
 
     if (req.body.joke !== "" && req.body.joke.trim().length > 0) {
         req.session.jokes.push(req.body.joke.trim());
     }
 
-    res.redirect('/');
+    res.redirect('/jokes');
 
 });
 
